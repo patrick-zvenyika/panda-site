0747febce0ea9db958202996e057c2ca6e76acfe
patrick-zvenyika <56692013+patrick-zvenyika@users.noreply.github.com>
2022-02-17 13:36:35 +0200

    final

--- panda/base/urls.py
+++ panda/base/urls.py
@@ -1,21 +1,24 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url 
 
 urlpatterns = [
-    path('login/', views.loginPage, name='login'),
-    path('logout/', views.logoutUser, name='logout'),
-    path('register/', views.registerPage, name='register'),
-    path('', views.home, name='home'),
-    
-    path('room/<str:pk>/', views.room, name='room'),
-    path('profile/<str:pk>/', views.userProfile, name='user-profile'),
-    path('create-room/', views.createRoom, name ='create-room'),
-    path('update-room/<str:pk>/', views.updateRoom, name ='update-room'),
-    path('delete-room/<str:pk>/', views.deleteRoom, name ='delete-room'),
-    path('delete-message/<str:pk>/', views.deleteMessage, name ='delete-message'),
-    path('delete-message/<str:pk>/', views.deleteMessage, name ='delete-message'),
-    path('update-profile/', views.updateUser, name ='update-profile'),
-    path('topics/', views.topicsPage, name ='topics'),
-     path('activity-feed/', views.activityPage, name ='activity'),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
 
+    path('', views.home, name="home"),
+    path('room/<str:pk>/', views.room, name="room"),
+    path('profile/<str:pk>/', views.userProfile, name="user-profile"),
+
+    path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
+    path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
+
+    path('update-user/', views.updateUser, name="update-user"),
+
+    path('topics/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
+   
 ]
